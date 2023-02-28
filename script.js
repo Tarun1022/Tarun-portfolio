58387861ef3abbfc069355361586b640c95b8e63
Tarun1022 <106371062+Tarun1022@users.noreply.github.com>
2023-02-28 15:00:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Digital Enterpreneur", "Helping people to earn money with a teamwork", "Assistant Supervisor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
